eb9051dd0408ead5722c6247c95aaa2ae30538ad
Joancy <mary.joancym@gmail.com>
2025-09-23 17:05:20 +0530

    New Python

---
+++ Listmethod.py
@@ -0,0 +1,35 @@
+# li = [20,48,38,7,6]
+# li.append(77)
+# print(li)
+# append: adds single element in the list
+# o/p- [20, 48, 38, 7, 6, 77]
+
+# list1=[33,6,8,1,99]
+# list1.extend([88,23,54])
+# print(list1)
+# extend: extends the list space and adds all element at the end of the list
+# o/p- [33, 6, 8, 1, 99, 88, 23, 54]
+ 
+# items=[102,74,86,33,54]
+# items.insert(4,90)
+# print(items)
+# insert-inserts the given element in the specified index.
+# 4th index the element 90 is inserted
+# o/p-[102, 74, 86, 33, 90, 54]
+
+# num=[42,65,87,22,54]
+# num.remove(87)
+# print(num)
+# remove - removes the specified value in the list, 
+# if the value is repeated it removes the first occurrence of a specified value.
+# op - [42, 65, 22, 54]
+
+# p=[234,42,75,86,21]
+# rem=p.pop(3)
+# print(rem)
+# pop (index=-1):removes and returns the element at the specified index,
+# o/p - 86
+# if no index is provided it removes and returns the last element
+# example: rem=p.pop()
+# o/p - 21
+
---
+++ Pizashop.py
@@ -0,0 +1,13 @@
+print("Welcome to Pizza hub")
+print("\n MENU")
+print("\n 1. Classic Cheese Pizza Small")
+print(" 2. Classic Cheese Pizza Large")
+Option=int(input("\n Enter your option: "))
+
+match Option:
+    case "1":
+        Qty=int(input("Enter the quantity: "))
+    case "2":
+        print("Enter the quantity: ")
+
+
